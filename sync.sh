9eee91f7bdc67294f61654e54b1a5a16a6147c03
Fusl Dash <root@hallowe.lt>
2017-08-15 04:03:28 +0000

    2017-08-15T04:03:25+00:00

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 while true; do
-	sleep 30m
 	TZ=Etc/UTC date -Is
 	cd /var/www/html/dokuwiki/
 	rm -f /var/www/html/dokuwiki/.git/index.lock
 	git add -A
 	git commit -m "$(TZ=Etc/UTC date -Is)" && git push --force origin master
+	sleep 30m
 done
 
