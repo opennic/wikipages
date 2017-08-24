48277300c32c3eb219c83cf19b04160d6b243a65
Fusl Dash <root@hallowe.lt>
2017-08-24 15:07:49 +0000

    2017-08-24T15:07:47+00:00

--- conf/plugins.local.php
+++ conf/plugins.local.php
@@ -6,7 +6,4 @@
  * NOTE: Plugins will not be added to this file unless there is a need to override a default setting. Plugins are
  *       enabled by default.
  */
-$plugins['authad'] = 0;
-$plugins['authpgsql'] = 0;
-$plugins['authpdo'] = 0;
-$plugins['authmysql'] = 0;
+$plugins['styling'] = 0;
